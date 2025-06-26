571ca18fb6bcea720bb3600aefe10fc0d8056e9c
Jakub Gawędzki <105167719+Gawendz@users.noreply.github.com>
2025-06-26 21:34:12 +0200

    Update etl.py

--- etl.py
+++ etl.py
@@ -13,14 +13,14 @@ def main():
              .master("local[*]")
              .getOrCreate())
 
-    # 1) pobierz dataset jako pandas, „?” → None
+    #dataset jako pandas, „?” → None
     pdf = (fetch_ucirepo(id=235).data.features
            .replace({"?": None, "": None}))
 
-    # 2) pandas → spark
+    # pandas → spark
     df = spark.createDataFrame(pdf)
 
-    # 3) rzutuj kolumny liczbowe + zamień NaN → null
+    #rzutowanie kolumn liczbowych +  NaN → null
     num = ["Global_active_power","Global_reactive_power",
            "Voltage","Global_intensity",
            "Sub_metering_1","Sub_metering_2","Sub_metering_3"]
@@ -28,19 +28,19 @@ def main():
         df = (df.withColumn(c, col(c).cast("double"))
                 .withColumn(c, when(col(c).isNaN(), None).otherwise(col(c))))
 
-    # 4) pełny timestamp i kolumna DATE
+    #pełny timestamp i kolumna DATE
     df = (df
           .withColumn("datetime",
               try_to_timestamp(concat_ws(" ", col("Date"), col("Time")),
                                lit("d/M/yyyy H:mm:ss")))
           .withColumn("date", to_date(col("Date"), "d/M/yyyy")))
 
-    # 5) odfiltruj wiersze bez poprawnej daty
+    #filtracja wierszy bez poprawnej daty
     bad = df.filter(col("date").isNull()).count()
     print(f"[clean] wiersze bez daty: {bad}")
     df = df.filter(col("date").isNotNull())
 
-    # 6) zapisz Parquet
+    #zapis Parquet
     (df.repartition(1)
        .write.mode("overwrite").parquet(DEST))
     print(f"[clean] zapisano {DEST}")
